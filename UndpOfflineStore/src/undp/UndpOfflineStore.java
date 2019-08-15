00a872c0361ebfacb386be028c2df206ac924042
Nebojsa <spitfirekg@gmail.com>
2019-08-15 11:09:13 +0200

    dodavanje interfejsa

--- UndpOfflineStore/src/undp/UndpOfflineStore.java
+++ UndpOfflineStore/src/undp/UndpOfflineStore.java
@@ -1,66 +1,90 @@
 
 package undp;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javax.swing.JOptionPane;
 
 public class UndpOfflineStore extends Application {
+        //dugmici za realizaciju pojedinih funkcija
+        Button btn1 = new Button("Prodaja");
+        Button btn2 = new Button("Nabavka");
+        Button btn3 = new Button("Izvestaji");
+        Button btn4 = new Button("Azuriranje");
+        
+        //Kreiranje HBox panela za popunjvenje scene
+        HBox hb1 = new HBox();
+        HBox hb2 = new HBox();
+        HBox hb3 = new HBox();
+        
+        //Kreiranje tekstualnih labela i fonta
+        Label lab1 = new Label("UNDP OfflineStore");
+        Font fotn = new Font(25);
+        
     
     @Override
     public void start(Stage primaryStage) {
         
-        //dugmici za realizaciju pojedinih funkcija
-        Button btn1 = new Button("Nabavka");
-        Button btn2 = new Button("Prodaja");
-        Button btn3 = new Button("Izvestaji");
-        Button btn4 = new Button("Azuriranje");
+        //Podesavanje velicine , izgleda i dodavanje nodova na Hbox
+        hb1.setMinSize(800, 120);
+        lab1.setId("headerLabel");
+        hb1.setId("headerBackground");
+        hb1.setAlignment(Pos.CENTER);
+        lab1.setFont(fotn);
+        hb1.getChildren().add(lab1);
+        
+        
+        ////Podesavanje velicine , izgleda i dodavanje nodova na Hbox;
+        FadeTransition fade= new FadeTransition();
+        fade.setDuration(Duration.millis(3500));
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.setAutoReverse(true);
+        fade.setNode(hb2);
+        fade.play();
+        hb2.setMinSize(800, 350);
+        hb2.setAlignment(Pos.CENTER);
+        hb2.setId("centerPicture");
+        
+        //Podesavanje velicine , izgleda i dodavanje nodova na Hbox
+        hb3.setMinSize(800, 130);
+        hb3.setAlignment(Pos.CENTER);
+        btn1.setId("buttonStyle");
+        btn2.setId("buttonStyle");
+        btn3.setId("buttonStyle");
+        btn4.setId("buttonStyle");
+        hb3.getChildren().addAll(btn1,btn2,btn3,btn4);
+        hb3.setSpacing(30);
+        hb3.setId("bottomStyle");
+        
+        //Kreiranje BorderPane-a za rasporedjivanje HBoc panela na sceni
+        BorderPane root = new BorderPane();
+        root.setTop(hb1);
+        root.setCenter(hb2);
+        root.setBottom(hb3);
+        
+        
 
-        //Nabavka komponenata
-        btn1.setOnAction(e->{
-            Platform.runLater(()->{
-                JOptionPane.showMessageDialog(null, "Jos uvek nije implementirano!");
-            });
-        });
         
-        //Prodaja komponenata
-        btn2.setOnAction(e->{
-            Platform.runLater(()->{
-                JOptionPane.showMessageDialog(null, "Jos uvek nije implementirano!");
-            });
-        });
-        
-        //Kreiranje izvestaja
-        btn3.setOnAction(e->{
-            Platform.runLater(()->{
-                JOptionPane.showMessageDialog(null, "Jos uvek nije implementirano!");
-            });
-        });
-        
-        //Azuriranje komponenata
-        btn4.setOnAction(e->{
-            Platform.runLater(()->{
-                JOptionPane.showMessageDialog(null, "Jos uvek nije implementirano!");
-            });
-        });
                 
         
-        GridPane root = new GridPane();
-        root.add(btn1, 0, 0);
-        root.add(btn2, 1, 0);
-        root.add(btn3, 2, 0);
-        root.add(btn4, 3, 0);
-        root.setPadding(new Insets(50,50,50,50));
-        root.setHgap(50);
         
-        Scene scene = new Scene(root, 600, 500);
-        
+        //Kreiranje scene,velicine,naziva i povezivanje sa css-om
+        Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("UNDP Offline Store");
+        scene.getStylesheets().addAll(this.getClass().getResource("styles.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
     }
