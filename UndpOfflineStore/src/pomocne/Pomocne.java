6a0bb33fc65bccbd7812ae484f8d1612f233290c
Nikola Timotijevic <nikolatx@gmail.com>
2019-11-19 22:07:47 +0100

    Zapoceto ispravljanje TableView kontrole sa slikom - dodavanje u tabelaOdabrano

--- UndpOfflineStore/src/pomocne/Pomocne.java
+++ UndpOfflineStore/src/pomocne/Pomocne.java
@@ -1,7 +1,9 @@
 
 package pomocne;
 
+import com.grupa1.dbconnection.PomocneDAO;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 
 public class Pomocne {
@@ -19,5 +21,21 @@ public class Pomocne {
         Platform.runLater( ()-> alert.showAndWait() );
     }
     
+    public static boolean ucitajProizvodjace(boolean proizvodjacOdabran, ObservableList opcijeProizvodjac, String item) {
+        if (!proizvodjacOdabran) {
+                opcijeProizvodjac.clear();
+                PomocneDAO.ispuniComboBoxZaProizvodjacaPoTipu(opcijeProizvodjac, item, false);
+                return true;
+        }
+        return false;
+    }
     
+    public static boolean ucitajTipove(boolean tipOdabran, ObservableList opcijeTip, String item) {
+        if (!tipOdabran) {
+                opcijeTip.clear();
+                PomocneDAO.ispuniComboBoxZaTipProizvodjaca(opcijeTip, item, false);
+                return true;
+        }
+        return false;
+    }
 }
